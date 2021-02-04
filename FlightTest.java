a598b7e80938e469575e8955f336f0369f984a1d
karthik <kc69@njit.edu>
2021-02-03 23:06:36 -0500

    this is my first commit

---
+++ FlightTest.java
@@ -0,0 +1,20 @@
+
+public class FlightTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Flight flight1, flight2, flight3;
+		
+		Flight f1 = new Flight("United Airlines","USA","India", 720);
+		Flight f2 = new Flight("Emirates","USA","Dubai",900);
+		Flight f3 = new Flight("Etihad Airways","USA","Saudi Arabia",360);
+		 
+		 System.out.println(f1);
+		 System.out.println(f2);
+		 System.out.println(f3);
+		 
+
+	}
+
+}
