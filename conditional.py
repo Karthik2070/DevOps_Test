764304f5c68620229a25a6d0502098721a65c9ca
karthik <kc69@njit.edu>
2021-02-04 21:49:47 -0500

    trying after pushing

---
+++ conditional.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 25 19:41:25 2020
+
+@author: Karthik Shekar
+"""
+
+
+marks=85
+if (marks>90 and marks<=100):
+    print('You have secured A+')
+elif(marks>80 and marks<=90):
+    print('You have secured B')
+elif(marks==85):
+    print('You have secured distinction')
+else:
+    print('Need not worry   ')
